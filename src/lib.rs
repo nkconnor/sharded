3742096835ddedffa6d455900ba8726ddfd30a3e
nkconnor <nicholas.k.connor@gmail.com>
2020-08-18 11:13:26 -0400

    Finangling w/ GATs

--- src/lib.rs
+++ src/lib.rs
@@ -1,39 +1,55 @@
-//! A hash-based RwLock for performant read/writes on hash-based
+//! A (to be) generic sharded lock mechanism for performant read/writes on hash-based
 //! collections like HashMap, HashSet.
 #![deny(unsafe_code)]
 #![allow(dead_code)]
 #![allow(unused_macros)]
+#![feature(generic_associated_types)]
+#![feature(in_band_lifetimes)]
 
 use parking_lot;
 use std::collections::hash_map::DefaultHasher;
 use std::collections::HashMap;
 use std::hash::Hash;
 
-//trait Lock {
-//    type Inner;
-//    fn new(t: Self::Inner) -> Self;
-//
-//    fn read(&self) -> Box<dyn Deref<Target = Self::Inner>>;
-//}
-//
-//
-//
-//    impl<T> Lock for RwLock<T> {
-//        type Inner = T;
-//
-//        fn new(t: T) -> Self {
-//            parking_lot::RwLock::new(t)
-//        }
-//
-//        fn read(&self) -> Box<dyn Deref<Target = T> + '_> {
-//            Box::new(self.read())
-//        }
-//    }
+pub trait Lock<T> {
+    #[rustfmt::skip]
+    type ReadGuard<'a> where T: 'a;
+    #[rustfmt::skip]
+    type WriteGuard<'a> where T: 'a;
 
+    fn new(t: T) -> Self;
+
+    fn write(&self) -> Self::WriteGuard<'_>;
+
+    fn read(&self) -> Self::ReadGuard<'_>;
+}
+
+impl<T> Lock<T> for parking_lot::RwLock<T> {
+    #[rustfmt::skip]
+    type ReadGuard<'b> where T: 'b = parking_lot::RwLockReadGuard<'b, T>;
+    #[rustfmt::skip]
+    type WriteGuard<'b> where T: 'b = parking_lot::RwLockWriteGuard<'b, T>;
+
+    fn new(t: T) -> Self {
+        parking_lot::RwLock::new(t)
+    }
+
+    fn read(&self) -> Self::ReadGuard<'_> {
+        self.read()
+    }
+
+    fn write(&self) -> Self::WriteGuard<'_> {
+        self.write()
+    }
+}
+
+/// Teases out the sharding key for example
+/// from an IntoIterator value.
 pub trait ExtractShardKey<K: Hash> {
     fn key(&self) -> &K;
 }
 
+/// Basic methods needing implemented for shard construction
 pub trait Collection<K: Hash, Value: ExtractShardKey<K>>:
     IntoIterator<Item = Value> + Clone
 {
@@ -64,18 +80,20 @@ impl<K: Hash + Clone + Eq, V: Clone> Collection<K, (K, V)> for HashMap<K, V> {
     }
 }
 
-pub struct Sharded<T> {
+/// The sharded lock collection.
+pub struct Shard<T> {
     shards: Vec<T>,
 }
 
-impl<T> Sharded<T> {
-    pub fn new<K, V, U>(inner: U) -> Sharded<parking_lot::RwLock<U>>
+impl<T> Shard<T> {
+    pub fn new<K, V, U, L>(inner: U) -> Shard<L>
     where
         K: Hash,
         V: ExtractShardKey<K>,
-        U: Collection<K, V>, // L: Lock<Inner = U>,
+        U: Collection<K, V>,
+        L: Lock<U>,
     {
-        let shard_count = 100;
+        let shard_count = 250;
 
         let mut shards = vec![U::with_capacity(inner.len() / shard_count); shard_count];
 
@@ -90,16 +108,13 @@ impl<T> Sharded<T> {
             }
         });
 
-        let shards = shards
-            .into_iter()
-            .map(|shard| parking_lot::RwLock::new(shard))
-            .collect();
+        let shards = shards.into_iter().map(|shard| L::new(shard)).collect();
 
-        Sharded { shards }
+        Shard { shards }
     }
 
     fn index<K: Hash>(k: &K) -> usize {
-        let shard_count = 100;
+        let shard_count = 250;
         use std::hash::Hasher;
         let mut s = DefaultHasher::new();
         k.hash(&mut s);
@@ -107,21 +122,41 @@ impl<T> Sharded<T> {
     }
 }
 
-impl<T> Sharded<parking_lot::RwLock<T>> {
-    pub fn write<K: Hash>(&self, k: &K) -> Option<parking_lot::RwLockWriteGuard<'_, T>> {
+//trait ShardOps {
+//    type K: Hash;
+//    type V;
+//    type U: Collection<Self::K, Self::V>;
+//    type L: Lock<Self::U>;
+//
+//    fn index(k: &Self::K) -> usize;
+//
+//    fn shards(&self) -> Vec<Self::L>;
+//
+//    fn write(&'a self, k: &Self::K) -> <<Self as ShardOps>::L as Lock<Self::U>>::WriteGuard<'a> {
+//        //let i = Self::index(k);
+//        //let l = *self.shards().get(i).unwrap();
+//        //l.write()
+//        //
+//        todo!()
+//    }
+//}
+
+impl<T> Shard<parking_lot::RwLock<T>> {
+    pub fn write<K: Hash>(&self, k: &K) -> parking_lot::RwLockWriteGuard<'_, T> {
         let i = Self::index(k);
-        self.shards.get(i).map(|lock| lock.write())
+        self.shards.get(i).map(|lock| lock.write()).unwrap()
     }
 
-    pub fn read<K: Hash>(&self, k: &K) -> Option<parking_lot::RwLockReadGuard<'_, T>> {
+    pub fn read<K: Hash>(&self, k: &K) -> parking_lot::RwLockReadGuard<'_, T> {
         let i = Self::index(k);
-        self.shards.get(i).map(|lock| lock.read())
+        self.shards.get(i).map(|lock| lock.read()).unwrap()
     }
 }
 
+#[macro_export]
 macro_rules! shard {
     ($($arg:tt)*) => {{
-        Sharded::<()>::new($($arg)*)
+        Shard::<()>::new($($arg)*)
     }}
 }
 
@@ -135,14 +170,10 @@ mod tests {
         let x = shard!(HashMap::new());
 
         x.write(&"key".to_string())
-            .unwrap()
             .insert("key".to_string(), "value".to_string());
 
         assert_eq!(
-            x.read(&"key".to_string())
-                .unwrap()
-                .get(&"key".to_string())
-                .unwrap(),
+            x.read(&"key".to_string()).get(&"key".to_string()).unwrap(),
             "value"
         );
     }
@@ -151,11 +182,11 @@ mod tests {
     fn hold_read_and_write() {
         let map = shard!(HashMap::new());
 
-        let mut write = map.write(&"abc".to_string()).unwrap();
+        let mut write = map.write(&"abc".to_string());
         write.insert("abc".to_string(), "asdf".to_string());
 
-        let _read = map.read(&"asdfas".to_string()).unwrap();
-        let _read_too = map.read(&"asdfas".to_string()).unwrap();
+        let _read = map.read(&"asdfas".to_string());
+        let _read_too = map.read(&"asdfas".to_string());
         assert!(_read.is_empty());
     }
 }
