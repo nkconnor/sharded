b35de3b47309ec60f4532e35c505b35d649592f3
nkconnor <nicholas.k.connor@gmail.com>
2020-08-19 20:52:07 -0400

    Add scratch work on bench viz

--- benchmarks/src/main.rs
+++ benchmarks/src/main.rs
@@ -72,7 +72,7 @@ fn main() {
     tracing_subscriber::fmt().json().flatten_event(true).init();
 
     let workloads = vec![
-        ("read_heavy", Mix::read_heavy()),
+        //        ("read_heavy", Mix::read_heavy()),
         ("write_heavy", Mix::insert_heavy()),
         ("update_heavy", Mix::update_heavy()),
         ("uniform", Mix::uniform()),
